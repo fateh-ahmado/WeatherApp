74f720845adb5e5169c52c51599ce0089271a9ff
Fateh Ahmado <ahmadofateh@gmail.com>
2026-08-03 15:35:28 +0200

    feat: legg til Identity med registrering, innlogging og autorisasjon

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using WeatherApp.Data;
 using WeatherApp.Services;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// Legger til dette for å bruke ApplicationDbContext med SQLite
+builder.Services.AddRazorPages();
 
 // Legger til dette for å bruke ApplicationDbContext med SQLite
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -17,7 +20,11 @@ builder.Services.AddHttpClient<LocationService>();
 // 
 builder.Services.AddHttpClient<WeatherForecastService>();
 //------------------------------------------------------------------
+//    Legger til dette for å bruke ApplicationDbContext med SQLite
+builder.Services.AddDefaultIdentity<Microsoft.AspNetCore.Identity.IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
+//------------------------------------------------------------------
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,8 +37,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
+//...................................................
+//Legger til autentisering og autorisasjon i applikasjonen. Dette er nødvendig for å håndtere brukerinnlogging og tilgangskontroll.
+app.UseAuthentication();
 app.UseAuthorization();
+//...................................................
 
 app.MapStaticAssets();
 
@@ -39,6 +49,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
+// Legger til rute for Razor Pages, som brukes for å håndtere brukerautentisering og autorisasjon i applikasjonen.    
+app.MapRazorPages();
 
 app.Run();
